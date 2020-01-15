0bcffbd9945b51907a06445b2985601661fbcce8
Romeno <romenowenogk@gmail.com>
2020-01-15 10:18:50 +0400

    navbar setup

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import About from './components/About';
-import Shop from './components/Shop';
 function App() {
   return (
     <main>
             <Switch>
                 <Route path="/" component={Home} exact />
                 <Route path="/about" component={About} />
-                <Route path="/shop" component={Shop} />
             </Switch>
         </main>
   );

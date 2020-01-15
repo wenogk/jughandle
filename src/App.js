2fe81a9a2427576180c3083e99572dc5dd67baea
Romeno <romenowenogk@gmail.com>
2020-01-15 10:03:38 +0400

    router stuff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-
+import Home from './components/Home';
+import About from './components/About';
+import Shop from './components/Shop';
 function App() {
   return (
-    <h1>hello world</h1>
+    <main>
+            <Switch>
+                <Route path="/" component={Home} exact />
+                <Route path="/about" component={About} />
+                <Route path="/shop" component={Shop} />
+            </Switch>
+        </main>
   );
 }
 

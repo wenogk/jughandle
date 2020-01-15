35a9988b043976c880ea289db96fa97930ce6d16
Romeno <romenowenogk@gmail.com>
2020-01-15 11:26:21 +0400

    sample textarea test using materialcss

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,9 @@ function Navbar() {
     <nav className="purple darken-2" role="navigation">
       <div className="nav-wrapper container"><a id="logo-container" href="/" className="brand-logo">Storify</a>
         <ul className="right hide-on-med-and-down">
-          <li><a href="/">Home</a></li>
-          <li><a href="/About">About</a></li>
           <li><a href="/Create">Create</a></li>
+          <li><a href="/About">About</a></li>
+
 
         </ul>
 
--- src/components/storyBuilderComponents/createStory.js
+++ src/components/storyBuilderComponents/createStory.js
@@ -6,6 +6,23 @@ function CreateStory() {
   <React.Fragment>
   <Navbar />
   <h1>Create page</h1>
+  <br />
+  <div className="row">
+      <form className="col s12">
+        <div className="row">
+          <div className="input-field col s6">
+            <input id="input_text" type="text" data-length="10" />
+            <label for="input_text">Input text</label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field col s12">
+            <textarea id="textarea2" className="materialize-textarea" data-length="120"></textarea>
+            <label for="textarea2">Textarea</label>
+          </div>
+        </div>
+      </form>
+    </div>
   <Footer />
   </React.Fragment>
   );

a81620ae1e4c87cd51573d45678616c6725c3ebd
Romeno <romenowenogk@gmail.com>
2020-01-15 10:40:07 +0400

    class to className renaming for react

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,38 +3,38 @@ import React from 'react'
 function Footer() {
   return (
 
-      <footer class="page-footer deep-purple darken-1">
-        <div class="container">
-          <div class="row">
-            <div class="col l6 s12">
-              <h5 class="white-text">Company Bio</h5>
-              <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+      <footer className="page-footer deep-purple darken-1">
+        <div className="container">
+          <div className="row">
+            <div className="col l6 s12">
+              <h5 className="white-text">Company Bio</h5>
+              <p className="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
 
 
             </div>
-            <div class="col l3 s12">
-              <h5 class="white-text">Settings</h5>
+            <div className="col l3 s12">
+              <h5 className="white-text">Settings</h5>
               <ul>
-                <li><a class="white-text" href="#!">Link 1</a></li>
-                <li><a class="white-text" href="#!">Link 2</a></li>
-                <li><a class="white-text" href="#!">Link 3</a></li>
-                <li><a class="white-text" href="#!">Link 4</a></li>
+                <li><a className="white-text" href="#!">Link 1</a></li>
+                <li><a className="white-text" href="#!">Link 2</a></li>
+                <li><a className="white-text" href="#!">Link 3</a></li>
+                <li><a className="white-text" href="#!">Link 4</a></li>
               </ul>
             </div>
-            <div class="col l3 s12">
-              <h5 class="white-text">Connect</h5>
+            <div className="col l3 s12">
+              <h5 className="white-text">Connect</h5>
               <ul>
-                <li><a class="white-text" href="#!">Link 1</a></li>
-                <li><a class="white-text" href="#!">Link 2</a></li>
-                <li><a class="white-text" href="#!">Link 3</a></li>
-                <li><a class="white-text" href="#!">Link 4</a></li>
+                <li><a className="white-text" href="#!">Link 1</a></li>
+                <li><a className="white-text" href="#!">Link 2</a></li>
+                <li><a className="white-text" href="#!">Link 3</a></li>
+                <li><a className="white-text" href="#!">Link 4</a></li>
               </ul>
             </div>
           </div>
         </div>
-        <div class="footer-copyright">
-          <div class="container">
-          Made by <a class="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+        <div className="footer-copyright">
+          <div className="container">
+          Made by <a className="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
           </div>
         </div>
       </footer>

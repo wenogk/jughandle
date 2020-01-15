ff6bdb7ef4c9eb16a8cfc26b8bf8e6ee197a827c
Romeno <romenowenogk@gmail.com>
2020-01-15 10:07:53 +0400

    about

---
+++ components/About.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function About() {
+  return (
+    <h1>THIS IS THE ABOUT</h1>
+  );
+}
--- src/App.js
+++ src/App.js
@@ -17,15 +17,5 @@ function App() {
   );
 }
 
-function Navbar() {
-  return (
-    <div>
-      <Link to="/">Home </Link>
-      <Link to="/about">About Us </Link>
-      <Link to="/shop">Shop Now </Link>
-    </div>
-  );
-};
-
 
 export default App;

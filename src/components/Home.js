567693154ae139661097cb4811609a40e6518f6f
Romeno <romenowenogk@gmail.com>
2020-01-15 10:38:45 +0400

    home content

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,60 @@ function Home() {
   return (
     <React.Fragment>
     <Navbar />
-    <h1>THIS IS THE HOME</h1>
+    <div className="section no-pad-bot" id="index-banner">
+
+      <br /><br />
+      <div className="typewriter">
+      <h1 className="header center hide-on-small-only" style={{color:"#4a148c"}}>Build choose-your-own-path experiences.</h1>
+      </div>
+      <h1 className="header center hide-on-med-and-up" style={{color:"#4a148c"}}>Build choose-your-own-path experiences.</h1>
+      <div className="container">
+        <div className="row center">
+          <h5 className="header col s12 light">You focus on the story, we'll handle the technical stuff.</h5>
+        </div>
+        <div className="row center">
+          <a  href="http://materializecss.com/getting-started.html" id="download-button" className="tooltipped btn-large waves-effect waves-light float-ease-in-out pulse" style={{background:"black"}} data-tooltip="No registrations or payments. We promise.">Create a story now</a>
+        </div>
+        <br /><br />
+
+      </div>
+    </div>
+
+
+    <div className="container">
+      <div className="section">
+
+        <div className="row">
+          <div className="col s12 m4">
+            <div className="icon-block">
+              <h2 className="center light-blue-text"><i className="material-icons">flash_on</i></h2>
+              <h5 className="center">Speeds up development</h5>
+
+              <p className="light">We did most of the heavy lifting for you to give you the ability to write stories with no coding knowledge.</p>
+            </div>
+          </div>
+
+          <div className="col s12 m4">
+            <div className="icon-block">
+              <h2 className="center light-blue-text"><i className="material-icons">ondemand_video</i></h2>
+              <h5 className="center">Video stories</h5>
+              <p className="light">Capability of building choose-your-own-path videos using streamable and youtube videos.</p>
+            </div>
+          </div>
+
+          <div className="col s12 m4">
+            <div className="icon-block">
+              <h2 className="center light-blue-text"><i className="material-icons">settings</i></h2>
+              <h5 className="center">HashStory Syntax</h5>
+
+              <p className="light">For those of you who like to have full control. You are able to use our own syntax to build choose-your-own-path experiences with no prior coding knowledge. The syntax is converted to a Javascript JSON object and then can be run.</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
     </React.Fragment>
   );
 }

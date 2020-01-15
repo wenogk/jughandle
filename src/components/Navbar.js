e8738531d1d7747ee11d6d016f3ab63bf54598b5
Romeno <romenowenogk@gmail.com>
2020-01-15 11:13:43 +0400

    Storify logo link to home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar() {
   return (
     <nav className="purple darken-2" role="navigation">
-      <div className="nav-wrapper container"><a id="logo-container" href="#" className="brand-logo">Storify</a>
+      <div className="nav-wrapper container"><a id="logo-container" href="/" className="brand-logo">Storify</a>
         <ul className="right hide-on-med-and-down">
           <li><a href="/">Home</a></li>
           <li><a href="/About">About</a></li>

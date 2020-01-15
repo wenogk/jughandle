e5fb7b4a66cccefefadca7e5f1f95cd987ab3171
Romeno <romenowenogk@gmail.com>
2020-01-15 14:45:34 +0400

    publish button

--- src/components/storyBuilderComponents/PathItemInput.js
+++ src/components/storyBuilderComponents/PathItemInput.js
@@ -1,8 +1,30 @@
 import React from 'react'
 
-function Footer() {
+function PathItemInput() {
   return (
-
+    <div className="row">
+     <div className="col s12 m12">
+       <div className="card white" style={{height:"auto"}}>
+         <div className="card-content">
+           <span className="card-title">Story Root</span>
+           <p>I am a very simple card. I am good at containing small bits of information.
+           I am convenient because I require little markup to use effectively.</p>
+           <div className="input-field col s12">
+             <textarea id="textarea2" className="materialize-textarea" data-length="120"></textarea>
+             <label for="textarea2">Start Path</label>
+           </div>
+           <br/>
+         </div>
+        <div className="center-align" style={{padding:"10px"}}>
+         <ul className="list-inline">
+       <li><a className="waves-effect waves-light btn-small red"><i className="material-icons right">queue</i>Add question</a></li>
+    <li><a className="waves-effect waves-light btn-small green"><i className="material-icons right">video_call</i>Add video</a></li>
+    <li><a className="waves-effect waves-light btn-small blue"><i className="material-icons right">collections</i>Add picture</a></li>
+       </ul>
+      </div>
+       </div>
+     </div>
+    </div>
   );
 }
 
--- src/components/storyBuilderComponents/createStory.js
+++ src/components/storyBuilderComponents/createStory.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Navbar from '../Navbar'
 import Footer from '../Footer'
-
+import PathItemInput from './PathItemInput'
 function addScript(src){
   var tag = document.createElement('script');
   tag.async = true;
@@ -22,30 +22,14 @@ function CreateStory() {
   <Navbar />
   <div className="container">
   <h1>Create Story</h1>
-  <div className="row">
-   <div className="col s12 m12">
-     <div className="card white" style={{height:"auto"}}>
-       <div className="card-content">
-         <span className="card-title">Story Root</span>
-         <p>I am a very simple card. I am good at containing small bits of information.
-         I am convenient because I require little markup to use effectively.</p>
-         <div className="input-field col s12">
-           <textarea id="textarea2" className="materialize-textarea" data-length="120"></textarea>
-           <label for="textarea2">Start Path</label>
-         </div>
-         <br/>
-       </div>
-      <div className="center-align" style={{padding:"10px"}}>
-       <ul className="list-inline">
-     <li><a className="waves-effect waves-light btn-small red"><i className="material-icons right">queue</i>Add question</a></li>
-<li><a className="waves-effect waves-light btn-small green"><i className="material-icons right">video_call</i>Add video</a></li>
-<li><a className="waves-effect waves-light btn-small blue"><i className="material-icons right">collections</i>Add picture</a></li>
-     </ul>
-    </div>
-     </div>
-   </div>
- </div>
+  <PathItemInput />
+  <PathItemInput />
+  <PathItemInput />
+  <div className="center-align"><br />
+  <a class="waves-effect waves-light btn-large"><i class="material-icons left">cloud</i>PUBLISH</a>
 
+  </div>
+    <br />  <br />
     </div>
   <Footer />
 

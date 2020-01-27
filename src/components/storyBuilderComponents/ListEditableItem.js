31e418d3e481cfc1a92953219883cc6c582383fd
Romeno <romenowenogk@gmail.com>
2020-01-27 20:13:55 +0400

    option title state

--- src/components/storyBuilderComponents/ListEditableItem.js
+++ src/components/storyBuilderComponents/ListEditableItem.js
@@ -3,7 +3,7 @@ import M from "materialize-css";
 import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 const ListEditableItem = ({ title, pathID }) => {
-const [options, setOptions] = useState([])
+const [optionTitle, setOptionTitle] = useState(title);
 return(
   <React.Fragment>
   <li key={index} className="collection-item"><div>Option {value} <a onClick={e => deleteOptionHandler(e,index)} className="secondary-content"><i className="material-icons red-text">delete_forever</i></a><a className="secondary-content"><i className="material-icons black-text">edit</i></a></div></li>

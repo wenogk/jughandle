45e87485d81e7a1023bc6b29082d5662d694847f
Romeno <romenowenogk@gmail.com>
2020-01-27 20:20:57 +0400

    edit mode bool state

--- src/components/storyBuilderComponents/ListEditableItem.js
+++ src/components/storyBuilderComponents/ListEditableItem.js
@@ -4,6 +4,10 @@ import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 const ListEditableItem = ({ title, pathID }) => {
 const [optionTitle, setOptionTitle] = useState(title);
+const [editMode, setEditMode] = useState(false);
+function toggleEditMode() {
+  setEditMode(!editMode);
+}
 return(
   <React.Fragment>
   <li key={index} className="collection-item"><div>Option {value} <a onClick={e => deleteOptionHandler(e,index)} className="secondary-content"><i className="material-icons red-text">delete_forever</i></a><a className="secondary-content"><i className="material-icons black-text">edit</i></a></div></li>

92dc19aeaf818ca7ec5867481af954b1a858768f
Romeno <romenowenogk@gmail.com>
2020-02-01 19:41:32 +0400

    treeview works

--- src/components/storyBuilderComponents/TreeView.js
+++ src/components/storyBuilderComponents/TreeView.js
@@ -1,45 +1,67 @@
 import React from 'react'
 import { Tree, TreeNode } from 'react-organizational-chart'
-let divStyle = {
-  color: 'black',
-  padding: '5px',
-  display: 'inline-block',
-  border: '5px solid black'
-};
+
 let obj = {}
 const TreeView = (pathArg) => {
   let paths = pathArg.paths
+
   function getLabelCode(text) {
+    let divStyle = {
+      color: '#311b92',
+      padding: '5px',
+      display: 'inline-block',
+      border: '5px solid #311b92'
+    };
     return <div style={divStyle}>{text}</div>;
   }
-  function getTreeCode(searchID) {
-    if(paths[searchID].options.length==0) {
-      let atr = <div style={divStyle}>{paths[searchID].title}</div>;
-      return <TreeNode label={atr} />;
-    }
-    else  {
-      let atr = <div style={divStyle}>{paths[searchID].title}</div>;
-      return (
-        <TreeNode label={atr} >
-            {paths[searchID].options.map((option, index) => (
-              <TreeNode label={getLabelCode(option.text)}>
-              {(getTreeCode(option.pathID))}
-              </TreeNode>
-            ))
-          }
-        </TreeNode>
-      );
 
+  function hasChildren(id) {
+    return (paths[id].options.length > 0)
+  }
+
+  function getOptionsCode(option) {
+    if(hasChildren(option.pathID)) {
+      return(
+        <TreeNode label={getLabelCode(option.text)}>
+          {(getTreeCode(option.pathID))}
+        </TreeNode>
+      )
+    } else {
+      return(
+        <TreeNode label={getLabelCode(option.text)} />
+      )
     }
+
+  }
+
+  function getTreeCode(searchID, prevSearchID = "") {
+    if(prevSearchID==searchID) {
+      return;
+    } else if(paths[searchID].options.length==0 && searchID!="root") {
+      return <TreeNode label={getLabelCode(paths[searchID].title)} />;
+    } else  {
+          return (
+            <React.Fragment>
+                {
+                  paths[searchID].options.map((option, index) => (
+                    <React.Fragment>
+                      {getOptionsCode(option)}
+                      </React.Fragment>
+                  ))
+                }
+              </React.Fragment>
+          );
+
+        }
 
   }
 
   return (
       <React.Fragment>
         <Tree
-        lineWidth={"10px"}
-        lineColor={"blue"}
-        label = {paths["root"].title}
+        lineWidth={"5px"}
+        lineColor={"black"}
+        label = {getLabelCode(paths["root"].title)}
         >
           {getTreeCode("root")}
         </Tree>

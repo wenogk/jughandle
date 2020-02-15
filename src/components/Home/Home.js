1b87ad5a0ef82a2e5d3aaa99d9b41fe0016d88e6
Romeno <romenowenogk@gmail.com>
2020-02-15 11:46:47 +0400

    smol installs

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -38,6 +38,14 @@ useEffect(() => {
 
 
     <div className="container">
+    <div class="plyr__video-embed" id="player">
+  <iframe
+      src="https://www.youtube.com/embed/bTqVqk7FSmY?origin=https://plyr.io&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1"
+      allowfullscreen
+      allowtransparency
+      allow="autoplay"
+  ></iframe>
+</div>
       <div className="section">
 
         <div className="row">
@@ -71,7 +79,9 @@ useEffect(() => {
       </div>
 
     </div>
-
+    <script>
+    const player = new Plyr('#player');
+    </script>
     <Footer />
     </React.Fragment>
   );

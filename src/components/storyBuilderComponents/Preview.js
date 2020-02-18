291b0a2103cf7f330d0c619b9c835c1c84a86b6b
Romeno <romenowenogk@gmail.com>
2020-02-18 18:15:39 +0400

    react player integration and option visibility when 95% of video is loaded

--- src/components/storyBuilderComponents/Preview.js
+++ src/components/storyBuilderComponents/Preview.js
@@ -1,15 +1,21 @@
 import React, { useState } from 'react'
 import {useSelector} from 'react-redux';
-
+import ReactPlayer from 'react-player'
 const Preview = () => {
   const PATHS = useSelector((state) => {return(state)});
   const [currentPathID, setCurrentPathID] = useState("root");
   const [previousPathID, setPreviousPathID] = useState("root");
+  const [percentagePlayed, setPercentagePlayed] = useState(0);
+
 
   function loadPath(idVal) {
+    setPercentagePlayed(0) //reset the percentage played
     setPreviousPathID(currentPathID)
     setCurrentPathID(idVal)
 
+  }
+  function videoProgressed({played, loaded, playedSeconds, loadedSeconds}) {
+      setPercentagePlayed(played);
   }
   let options = [];
   for(let i in PATHS[currentPathID].options) {
@@ -21,7 +27,7 @@ const Preview = () => {
     options.push(<a onClick={() => {loadPath("root")}}>Start Over</a>);
   }
 
-
+if(PATHS[currentPathID].video =="") {
   return (
     <React.Fragment>
     <div className="container">
@@ -40,6 +46,30 @@ const Preview = () => {
   </div>
     </React.Fragment>
   );
+} else {
+  console.log("video: " + PATHS[currentPathID].video)
+  return (
+    <React.Fragment>
+    <div className="container">
+    <div class="row ">
+    <div class="col s12 m12">
+      <div class="card blue-grey darken-1">
+        <div class="card-content white-text">
+          <ReactPlayer playing={true} url={PATHS[currentPathID].video} onProgress={videoProgressed} />
+        </div>
+        <div class="card-action">
+          {(percentagePlayed>0.95) ? options : ""}
+          <br />
+          <p style={{color:"white"}}>{percentagePlayed}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+    </React.Fragment>
+  );
+}
+
 };
 /*
 <pre>

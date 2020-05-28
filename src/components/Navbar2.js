e6c9c656963f18fee88bb4a881ed6a4a581a7582
wenogk <drf325@nyu.edu>
2020-05-28 19:39:32 +0400

    Navbar 2 updates

--- src/components/Navbar2.js
+++ src/components/Navbar2.js
@@ -1,25 +1,35 @@
-import React from 'react'
+import React, {useContext} from 'react';
 import { Link } from "react-router-dom";
-
+import { Redirect } from 'react-router';
+import {UserContext} from '../UserContext';
 
 function Navbar2() {
+  const {user, setUser} = useContext(UserContext);
   return (
     //<div className="navbar-fixed">
     <nav className="nav-extended purple darken-2" role="navigation">
       <div className="nav-wrapper container"><Link id="logo-container" to="/" className="brand-logo">rivermouth</Link>
-        <ul className="right hide-on-med-and-down">
-          <li><Link to="/Create">Create</Link></li>
-          <li><Link to="/Edit">Edit</Link></li>
-          <li><Link to="/About">About</Link></li>
+      <ul className="right hide-on-med-and-down">
 
+        <li><Link to="/about">About</Link></li>
+        {user.loggedIn &&
+          <li><Link to="/">{(user.name).split(' ')[0]}</Link></li>
+        }
+        {user.loggedIn &&
+          <li><Link to="/logout">Logout</Link></li>
+        }
+      </ul>
 
-        </ul>
+      <ul id="nav-mobile" className="sidenav">
 
-        <ul id="nav-mobile" className="sidenav">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/About">About</Link></li>
-          <li><Link to="/Create">Create</Link></li>
-        </ul>
+        <li><Link to="/about">About</Link></li>
+        {user.loggedIn &&
+          <li><Link to="/">Your stories</Link></li>
+        }
+        {user.loggedIn &&
+          <li><Link to="/logout">Logout</Link></li>
+        }
+      </ul>
         <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
       </div>
       <div class="nav-content ">
